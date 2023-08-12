c311f5d8c5365bced4637ae8d1a1e86d60f8cbe2
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-08-12 17:31:11 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y;
+   printf("enter x ?:");
+   scanf("%d",&x);
+   printf("enter y ?:");
+   scanf("%d",&y);
+   if(x>=0&&y>=0)printf("the numbers have same sign");
+   else printf("the numbers does'y have same sign");
+
+
+    return 0;
+}
